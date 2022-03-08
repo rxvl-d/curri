b1a6fcc799d7b3f4cf97dace34919488229139d1
Ratan Sebastian <ratanjagan@gmail.com>
2022-03-08 09:34:03 +0100

    move deployment stuff out

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=10
-srun python curry/main.py $@
--- run_batch.sh
+++
@@ -1,2 +0,0 @@
-sbatch -J level_pred_1 -e level_pred_1.err.log -o level_pred_1.out.log run.sh 10
-
