9a4e62cb7ee204cb79c64cb606f61faf84ce0ba9
Ratan Sebastian <ratanjagan@gmail.com>
2022-03-10 17:47:45 +0100

    settings for exploring state specific classifiers

--- gen_model_conf.py
+++ gen_model_conf.py
@@ -4,17 +4,20 @@ import json
 
 conf = []
 
-models = ['xgbClassifier', 'xgbOrdinalClassifier', 'xgbCoarseGrainedClassifier']
-vec_types = [
-    'kw', 'babelkw', 'wikikw', 'st', 'tfidf',
-    'babelkw+wikikw', 'st+wikikw', 'st+babelkw',
-    'babelkw+kw', 'wikikw+kw', 'wikikw+babelkw+kw']
+# models = ['xgbClassifier', 'xgbOrdinalClassifier', 'xgbCoarseGrainedClassifier']
+models = ['xgbClassifier']
+# vec_types = [
+#     'kw', 'babelkw', 'wikikw', 'st', 'tfidf',
+#     'babelkw+wikikw', 'st+wikikw', 'st+babelkw',
+#     'babelkw+kw', 'wikikw+kw', 'wikikw+babelkw+kw']
+vec_types = ['kw', 'babelkw', 'wikikw', 'st']
 lander = ['baden-wuerttemberg', 'bayern', 'berlin', 'brandenburg', 'bremen',
        'hamburg', 'hessen', 'mecklenburg-vorpommern',
        'nordrhein-westfalen', 'saarland', 'sachsen', 'sachsen-anhalt',
        'schleswig-holstein', 'thueringen', 'niedersachsen',
        'rheinland-pfalz']
-filtered = [True, False]
+# filtered = [True, False]
+filtered = [False]
 
 for model in models:
     for vec_type in vec_types:
