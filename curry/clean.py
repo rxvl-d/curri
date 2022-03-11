b058814361989ee3cdc69d471d961a52ac6deae2
Ratan Sebastian <ratanjagan@gmail.com>
2022-03-11 16:15:06 +0100

    alter cleaning to get more text

--- curry/clean.py
+++ curry/clean.py
@@ -5,20 +5,6 @@ import trafilatura
 
 
 class Cleaner:
-    def __init__(self, cleaner_type):
-        assert cleaner_type in {'tf', 'rd'}
-        self.cleaner_type = cleaner_type
-
-    def trafilatura_lines(self, html_content):
-        tf = trafilatura.extract(html_content,
-                                   output_format='json',
-                                   include_comments=False,
-                                   include_images=True,
-                                   include_links=True,
-                                   include_tables=True,
-                                   with_metadata=True)
-        return json.loads(tf)['raw_text'].split('.')
-
     def readability_lines(self, html_content):
         return [line['text'] for line in readabilipy.simple_json_from_html_string(html_content)['plain_text']]
 
@@ -32,9 +18,9 @@ class Cleaner:
         return '. '.join(accepted_lines)
 
     def clean(self, html_content):
-        if self.cleaner_type == 'tf':
-            return self.remove_equations(self.trafilatura_lines(html_content))
-        elif self.cleaner_type == 'rd':
-            return self.remove_equations(self.readability_lines(html_content))
-        else:
-            raise Exception('boom')
+        main_content = trafilatura.extract(html_content, favor_recall=True)
+        main_lines = []
+        for line in self.readability_lines(html_content):
+            if line in main_content:
+                main_lines.append(line)
+        return self.remove_equations(main_lines)
